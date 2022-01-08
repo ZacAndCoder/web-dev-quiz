a5309b0567e6c60ebc39afaf49419aff8965071c
ZacAndCoder <91081344+ZacAndCoder@users.noreply.github.com>
2022-01-08 12:22:53 -0500

    Remove unnecessary variables and add functions
    I wrote functions that trigger different transitions when changing between questions. I used setTimeout() to properly time these transitions. I used d3.js to create the back button and next button because d3 makes it easy to create triangles. I also disabled these buttons while animations are playing.

--- Quiz.js
+++ Quiz.js
@@ -1,22 +1,43 @@
-window.addEventListener("load", function() {
-  document.querySelector("body").classList.add("animate-page-load");
-});
-
 var currentQuestion = document.querySelector("#current-question"),
     nextButton = document.querySelector("#next-button"),
     backButton = document.querySelector("#back-button"),
     questions = document.querySelectorAll(".question"),
     answers = document.querySelectorAll(".answer"),
-    correctAnswers = 0;
+    numCorrectAnswers = 0,
+    numIncorrectAnswers = 5;
+
+//This function will be used to disable buttons while transitional animations are playing
+const disableButtons = function() {
+  nextButton.disabled = true;
+  backButton.disabled = true;
+}
+
+//This function will be used to enable buttons after animations have finished
+const enableButtons = function() {
+  nextButton.disabled = false;
+  backButton.disabled = false;
+}
 
 nextButton.addEventListener("click", function() {
+  disableButtons();
   if (currentQuestion.innerText == "Question 5 of 5") {
-    chosenAnswers.forEach(item => {
-      if (item.className == "correct") {
-        correctAnswers++;
-      }
-    });
-    currentQuestion.innerText = "Results";
+    let animateFinishQuiz = setTimeout(function() {
+      document.querySelector("body").classList.add("animate-body-color");
+      nextButton.classList.add("animate-body-color");
+      backButton.classList.add("animate-body-color");
+      answers.forEach(item => {
+        item.classList.add("animate-finish-quiz");
+      });
+    }, 0);
+    let animateShowResults = setTimeout(function() {
+      questions[4].style.display = "none";
+      document.querySelector("#results").style.display = "block";
+      document.querySelector("#results").classList.add("animate-fade-in");
+      nextButton.style.display = "none";
+      backButton.style.display = "none";
+      currentQuestion.style.display = "none";
+      document.querySelector("p").innerText = "You correctly answered " + numCorrectAnswers + " out of 5 questions.";
+    }, 1000);
   } else {
     //Execute animation and display different question
     let questionNumber = currentQuestion.innerHTML.match(/\d/);
@@ -36,15 +57,80 @@ nextButton.addEventListener("click", function() {
       questionNumber++;
       currentQuestion.innerText = "Question " + questionNumber + " of 5";
     }, 1000);
+    let allowButtonClick = setTimeout(function() {
+      enableButtons();
+    }, 1500);
   }
 });
 
+backButton.addEventListener("click", function() {
+  disableButtons();
+  if (currentQuestion.innerText == "Question 1 of 5" || currentQuestion.innerText == "Results") {
+    return;
+  } else {
+    //Execute animation and display different question
+    let questionNumber = currentQuestion.innerHTML.match(/\d/);
+    let animateSinkIn = setTimeout(function() {
+      answers.forEach(item => {
+        item.classList.remove("animate-emerge");
+        item.classList.add("animate-sink-in");
+      });
+    }, 0);
+    let animateEmerge = setTimeout(function() {
+      questions[questionNumber - 1].style.display = "none";
+      questions[questionNumber - 2].style.display = "block";
+      answers.forEach(item => {
+        item.classList.add("animate-emerge");
+        item.classList.remove("animate-sink-in");
+      });  
+      questionNumber--;
+      currentQuestion.innerText = "Question " + questionNumber + " of 5";
+    }, 1000);
+    let allowButtonClick = setTimeout(function() {
+      enableButtons();
+    }, 1500);
+  }
+});
+
+const correctAnswers = ["Application Programming Interface", "version control system", "2", "JavaScript Object Notation", "back-end"];
 var chosenAnswers = [];
+var answerResults = document.querySelectorAll("b");
 
 //Add chosen answer to array
 answers.forEach(item => {
   item.addEventListener("click", function() {
-    chosenAnswers.push(item);
-    item.style.border = "2px solid orange";
+    item.style.color = "#121212";
+    if (correctAnswers.includes(item.innerHTML)) {
+      numCorrectAnswers++;
+      numIncorrectAnswers--;
+      chosenAnswers.push(item.innerHTML);
+    }
+    answerResults.forEach(item => {
+      if (chosenAnswers.includes(item.innerHTML)) {
+        item.style.color = "green";
+      } else {
+        item.style.color = "red";
+      }
+    });
+    item.parentNode.childNodes.forEach(item => {
+      item.disabled = true;
+    });
+    return false;
   });
 });
+
+const leftArrow = d3.symbol().type(d3.symbolTriangle).size(350);
+
+d3.select("#left-arrow")
+  .append("path")
+  .attr("d", leftArrow)
+  .attr("fill", "#121212")
+  .attr("transform", "translate(32, 25) rotate(-90)");
+
+const rightArrow = d3.symbol().type(d3.symbolTriangle).size(350);
+
+d3.select("#right-arrow")
+  .append("path")
+  .attr("d", rightArrow)
+  .attr("fill", "#121212")
+  .attr("transform", "translate(13, 25) rotate(90)");
